569cf658752ddb90cb6e45bb79cfa6252f3cca86
FrancoisN7 <ftestu19@gmail.com>
2023-05-30 22:48:50 +0200

    suppr des fichiers lourds

---
+++ ModGeo/TP2/Assets/Scripts/CalculHodographe.cs
@@ -0,0 +1,158 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CalculHodographe : MonoBehaviour
+{
+    // Nombre de subdivision dans l'algo de DCJ
+    public int NombreDeSubdivision = 3;
+    // Liste des points composant la courbe de l'hodographe
+    private List<Vector3> ListePoints = new List<Vector3>();
+    // Donnees i.e. points cliqués
+
+    public GameObject Donnees;
+    public GameObject particle;
+
+    //////////////////////////////////////////////////////////////////////////
+    // fonction : DeCasteljauSub                                            //
+    // semantique : renvoie la liste des points composant la courbe         //
+    //              approximante selon un nombre de subdivision données     //
+    // params : - List<float> X : abscisses des point de controle           //
+    //          - List<float> Y : odronnees des point de controle           //
+    //          - int nombreDeSubdivision : nombre de subdivision           //
+    // sortie :                                                             //
+    //          - (List<float>, List<float>) : liste des abscisses et liste //
+    //            des ordonnées des points composant la courbe              //
+    //////////////////////////////////////////////////////////////////////////
+    (List<float>, List<float>) DeCasteljauSub(List<float> X, List<float> Y, int nombreDeSubdivision)
+    { 
+        // Début de l'algorithme
+        if (nombreDeSubdivision <= 0){
+            return (X, Y);
+        }
+
+        else{
+            // TODO !!
+            int n = X.Count;
+            List<float> LcurrX = new List<float>();
+            List<float> LcurrY = new List<float>();
+            List<float> RcurrX = new List<float>();
+            List<float> QcurrX = new List<float>();
+            List<float> RcurrY = new List<float>();
+            List<float> QcurrY = new List<float>();
+            
+            // Initialisation de R et Q
+            QcurrX.Add(X[0]);
+            QcurrY.Add(Y[0]);
+            RcurrX.Add(X[n-1]);
+            RcurrY.Add(Y[n-1]);
+
+            // Initialisation de Lcurr
+            for (int i=0 ; i<n ; i++){
+                LcurrX.Add(X[i]);
+                LcurrY.Add(Y[i]);
+            }
+
+            while(n - 1 > 0){
+                for (int i=1 ; i<n ; i++){
+                    LcurrX[i-1] = (LcurrX[i-1] + LcurrX[i])/2;
+                    LcurrY[i-1] = (LcurrY[i-1] + LcurrY[i])/2;   
+                }
+                n -= 1;
+                QcurrX.Add(LcurrX[0]);
+                QcurrY.Add(LcurrY[0]);
+                RcurrX.Add(LcurrX[n-1]);
+                RcurrY.Add(LcurrY[n-1]);
+            }
+
+            // Reverse Rcurr
+            RcurrX.Reverse();
+            RcurrY.Reverse();
+
+            // Return la concaténation
+            List<float> L1X = new List<float>();
+            List<float> L1Y = new List<float>();
+            List<float> L2X = new List<float>();
+            List<float> L2Y = new List<float>();
+
+            (L1X, L1Y) = DeCasteljauSub(QcurrX, QcurrY, nombreDeSubdivision-1);
+            (L2X, L2Y) = DeCasteljauSub(RcurrX, RcurrY, nombreDeSubdivision-1);
+            L1X.AddRange(L2X);
+            L1Y.AddRange(L2Y);
+            return (L1X,L1Y);
+        }
+    }
+
+    //////////////////////////////////////////////////////////////////////////
+    // fonction : Hodographe                                                //
+    // semantique : renvoie la liste des vecteurs vitesses entre les paires //
+    //              consécutives de points de controle                      //
+    //              approximante selon un nombre de subdivision données     //
+    // params : - List<float> X : abscisses des point de controle           //
+    //          - List<float> Y : odronnees des point de controle           //
+    //          - float Cx : offset d'affichage en x                        //
+    //          - float Cy : offset d'affichage en y                        //
+    // sortie :                                                             //
+    //          - (List<float>, List<float>) : listes composantes des       //
+    //            vecteurs vitesses sous la forme (Xs,Ys)                   //
+    //////////////////////////////////////////////////////////////////////////
+    (List<float>, List<float>) Hodographe(List<float> X, List<float> Y, float Cx = 1.5f, float Cy = 0.0f)
+    {
+        List<float> XSortie = new List<float>();
+        List<float> YSortie = new List<float>();
+
+        // TODO !!
+        // Initialisation de Xsortie et Ysortie
+        int n = X.Count;
+        for (int i=1 ; i<n ; i++){
+            XSortie.Add(X[i] - X[i-1]);
+            YSortie.Add(Y[i] - Y[i-1]);
+        }
+
+        Debug.Log(XSortie.Count);
+        
+        return (XSortie, YSortie);
+    }
+
+    //////////////////////////////////////////////////////////////////////////
+    //////////////////////////// NE PAS TOUCHER //////////////////////////////
+    //////////////////////////////////////////////////////////////////////////
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            Instantiate(particle, new Vector3(1.5f, -4.0f, 0.0f), Quaternion.identity);
+            var ListePointsCliques = GameObject.Find("Donnees").GetComponent<Points>();
+            if (ListePointsCliques.X.Count > 0)
+            {
+                List<float> XSubdivision = new List<float>();
+                List<float> YSubdivision = new List<float>();
+                List<float> dX = new List<float>();
+                List<float> dY = new List<float>();
+                
+                (dX, dY) = Hodographe(ListePointsCliques.X, ListePointsCliques.Y);
+
+                (XSubdivision, YSubdivision) = DeCasteljauSub(dX, dY, NombreDeSubdivision);
+                for (int i = 0; i < XSubdivision.Count; ++i)
+                {
+                    ListePoints.Add(new Vector3(XSubdivision[i], -4.0f, YSubdivision[i]));
+                }
+            }
+
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.blue;
+        for (int i = 0; i < ListePoints.Count - 1; ++i)
+        {
+            Gizmos.DrawLine(ListePoints[i], ListePoints[i + 1]);
+        }
+    }
+}
---
+++ ModGeo/TP3/Assets/Scripts/CourbesFermees.cs
@@ -0,0 +1,130 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CourbesFermees : MonoBehaviour
+{
+    // Liste des points composant la courbe
+    private List<Vector3> ListePoints = new List<Vector3>();
+    // Donnees i.e. points cliqués
+    public GameObject Donnees;
+    // Coordonnees des points composant le polygone de controle
+    private List<float> PolygoneControleX = new List<float>();
+    private List<float> PolygoneControleY = new List<float>();
+
+    // degres des polynomes par morceaux
+    public int degres = 5;
+    // nombre d'itération de subdivision
+    public int nombreIteration = 5;
+
+    //////////////////////////////////////////////////////////////////////////
+    // fonction : subdivise                                                 //
+    // semantique : réalise nombreIteration subdivision pour des polys de   //
+    //              degres degres                                           //
+    // params : - List<float> X : abscisses des point de controle           //
+    //          - List<float> Y : odronnees des point de controle           //
+    // sortie :                                                             //
+    //          - (List<float>, List<float>) : points de la courbe          //
+    //////////////////////////////////////////////////////////////////////////
+    (List<float>, List<float>) subdivise(List<float> X, List<float> Y) {
+        List<float> Xr = new List<float>();
+        List<float> Yr = new List<float>();
+
+        List<float> Xres = new List<float>();
+        List<float> Yres = new List<float>();
+        int n = X.Count;
+        for (int k=0 ; k<n ; k++){
+            Xres.Add(X[k]);
+            Yres.Add(Y[k]);
+        }
+
+        float temp_prem_x = X[0];
+        float temp_prem_y = Y[0];
+        for (int j = 0; j<degres; j++){
+            //Duplication
+            List<float> Xresint = new List<float>();
+            List<float> Yresint = new List<float>();
+            for (int k=0 ; k<n ; k++){
+                Xresint.Add(Xres[k]);
+                Xresint.Add(Xres[k]);
+                Yresint.Add(Yres[k]);
+                Yresint.Add(Yres[k]);
+            }
+
+            n = Xresint.Count;
+            Debug.Log(n);
+            //Calcul des milieux
+            for (int i=1 ; i<n ; i++){
+                Xresint[i-1] = (Xresint[i-1] + Xresint[i])/2;
+                Yresint[i-1] = (Yresint[i-1] + Yresint[i])/2;
+            }
+            //Calcul du dernier point
+            Debug.Log(n-1);
+            Xresint[n-1] = (Xresint[n-1] + temp_prem_x)/2;
+            Yresint[n-1] = (Yresint[n-1] + temp_prem_y)/2;
+            temp_prem_x = Xresint[0];
+            temp_prem_y = Yresint[0];
+
+            for (int k=0 ; k<Xres.Count ; k++){
+                Xres.Add(Xres[k]);
+                Yres.Add(Yres[k]);
+            }
+
+            for (int k=0 ; k<Xres.Count ; k++){
+                Xres[k] = Xresint[k];
+                Yres[k] = Yresint[k];
+            }
+        }
+        
+        return (Xr, Yr);
+    }
+
+    //////////////////////////////////////////////////////////////////////////
+    //////////////////////////// NE PAS TOUCHER //////////////////////////////
+    //////////////////////////////////////////////////////////////////////////
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            var ListePointsCliques = GameObject.Find("Donnees").GetComponent<Points>();
+            if (ListePointsCliques.X.Count > 0)
+            {
+                for (int i = 0; i < ListePointsCliques.X.Count; ++i)
+                {
+                    PolygoneControleX.Add(ListePointsCliques.X[i]);
+                    PolygoneControleY.Add(ListePointsCliques.Y[i]);
+                }
+                
+                List<float> Xres = new List<float>();
+                List<float> Yres = new List<float>();
+
+                (Xres, Yres) = subdivise(ListePointsCliques.X,ListePointsCliques.Y);
+                for (int i = 0; i < Xres.Count; ++i) {
+                    ListePoints.Add(new Vector3(Xres[i],-4.0f,Yres[i]));
+                }
+            }
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        for (int i = 0; i < PolygoneControleX.Count - 1; ++i)
+        {
+            Gizmos.DrawLine(new Vector3(PolygoneControleX[i],-4.0f, PolygoneControleY[i]), new Vector3(PolygoneControleX[i + 1], -4.0f, PolygoneControleY[i + 1]));
+        }
+        if (PolygoneControleX.Count > 0 ) {
+            Gizmos.DrawLine(new Vector3(PolygoneControleX[PolygoneControleX.Count - 1],-4.0f, PolygoneControleY[PolygoneControleY.Count - 1]), new Vector3(PolygoneControleX[0], -4.0f, PolygoneControleY[0]));
+        }
+
+        Gizmos.color = Color.blue;
+        for (int i = 0; i < ListePoints.Count - 1; ++i)
+        {
+            Gizmos.DrawLine(ListePoints[i], ListePoints[i + 1]);
+        }
+        if (ListePoints.Count > 0 ) {
+            Gizmos.DrawLine(ListePoints[ListePoints.Count - 1], ListePoints[0]);
+        }
+    }
+}
